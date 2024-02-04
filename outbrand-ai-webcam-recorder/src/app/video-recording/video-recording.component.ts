4e03a75fc46f1f04a6d56635893e88541d8026ef
moronizacmel <moronizacmel@hotmail.com>
2024-02-04 00:13:01 -0700

    Recording init

--- outbrand-ai-webcam-recorder/src/app/video-recording/video-recording.component.ts
+++ outbrand-ai-webcam-recorder/src/app/video-recording/video-recording.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
+
 
 @Component({
   selector: 'app-video-recording',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './video-recording.component.html',
   styleUrl: './video-recording.component.css'
 })
@@ -12,9 +13,89 @@ import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 
 export class VideoRecordingComponent implements OnInit {
 
+  @ViewChild('recordedVideo') recordVideoElementRef!: ElementRef;
+  @ViewChild('video') videoElementRef!: ElementRef;
+
+  videoElement!: HTMLVideoElement;
+  recordVideoElement!: HTMLVideoElement;
+  mediaRecorder: any;
+  recordedBlobs!: Blob[];
+  isRecording: boolean = false;
+  downloadUrl!: string;
+  stream!: MediaStream;
+
   constructor() { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: {
+          width: 360
+        }
+      })
+      .then(stream => {
+        this.videoElement = this.videoElementRef.nativeElement;
+        this.recordVideoElement = this.recordVideoElementRef.nativeElement;
+  
+        this.stream = stream;
+        this.videoElement.srcObject = this.stream;
+      });
+  }
+
+  startRecording() {
+    this.recordedBlobs = [];
+    let options: any = { mimeType: 'video/webm' };
+
+    try {
+      this.mediaRecorder = new MediaRecorder(this.stream, options);
+    } catch (err) {
+      console.log(err);
+    }
+
+    this.mediaRecorder.start(); // collect 100ms of data
+    this.isRecording = !this.isRecording;
+    this.onDataAvailableEvent();
+    this.onStopRecordingEvent();
+  }
+
+  stopRecording() {
+    this.mediaRecorder.stop();
+    this.isRecording = !this.isRecording;
+    console.log('Recorded Blobs: ', this.recordedBlobs);
+  }
+
+  playRecording() {
+    if (!this.recordedBlobs || !this.recordedBlobs.length) {
+      console.log('cannot play.');
+      return;
+    }
+    this.recordVideoElement.play();
+  }
+
+  onDataAvailableEvent() {
+    try {
+      this.mediaRecorder.ondataavailable = (event: any) => {
+        if (event.data && event.data.size > 0) {
+          this.recordedBlobs.push(event.data);
+        }
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  onStopRecordingEvent() {
+    try {
+      this.mediaRecorder.onstop = (event: Event) => {
+        const videoBuffer = new Blob(this.recordedBlobs, {
+          type: 'video/webm'
+        });
+        this.downloadUrl = window.URL.createObjectURL(videoBuffer); // you can download with <a> tag
+        this.recordVideoElement.src = this.downloadUrl;
+      };
+    } catch (error) {
+      console.log(error);
+    }
   }
 
 }
